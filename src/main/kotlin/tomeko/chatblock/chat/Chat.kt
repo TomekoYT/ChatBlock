44d96c1f1577676294601351048322f557b06c4d
TomekoYT <tomekoyt@interia.pl>
2026-03-30 21:39:48 +0200

    switch block receiving messages from java to kotlin

---
+++ src/main/kotlin/tomeko/chatblock/chat/Chat.kt
@@ -0,0 +1,50 @@
+package tomeko.chatblock.chat
+
+import net.minecraftforge.client.event.ClientChatReceivedEvent
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import tomeko.chatblock.config.ChatBlockConfig
+import java.util.Locale
+
+class Chat {
+    @SubscribeEvent
+    fun onChatReceive(event: ClientChatReceivedEvent) {
+        if (event.type.toInt() == 2 || event.message == null) {
+            return
+        }
+
+        var message = event.message.getUnformattedText()
+
+        if (ChatBlockConfig.blockReceivingIgnoreFormatting) {
+            message = removeFormatting(message)
+        }
+
+        for (macro in ChatBlockConfig.messagesToBlockReceiving) {
+            if (!macro.enabled) continue
+
+            val messageToHide = macro.text
+            if (messageToHide.isEmpty()) continue
+
+            if ((ChatBlockConfig.blockReceivingCaseSensitive && message.contains(messageToHide))
+                || (!ChatBlockConfig.blockReceivingCaseSensitive && message.lowercase(Locale.getDefault()).contains(
+                    messageToHide.lowercase(
+                        Locale.getDefault()
+                    )
+                ))
+            ) {
+                event.setCanceled(true)
+                return
+            }
+        }
+    }
+
+    private fun removeFormatting(string: String): String {
+        return string.replace("§.".toRegex(), "")
+    }
+
+    companion object {
+        fun register() {
+            MinecraftForge.EVENT_BUS.register(Chat())
+        }
+    }
+}
